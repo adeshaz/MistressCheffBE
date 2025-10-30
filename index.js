8a4b65011ffda70bd081414b62a3fa3dcfad47e4
adeshaz <hafizadegbite@gmail.com>
2025-10-30 13:38:32 +0100

    changed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express(); // ✅ Initialize first
 // ✅ CORS setup
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174 ","https://misstresschef-dl3i.vercel.app/"],
+    origin: ["http://localhost:5173", "http://localhost:5174 ","https://misstresschef-dl3i.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
